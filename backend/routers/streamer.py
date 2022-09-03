16c5c0881adb618d5da2e5b8ff3d07c5b26baab0
zhx281 <xiao1@BOKU56>
2022-09-02 18:29:59 -0600

    added comments

--- backend/routers/streamer.py
+++ backend/routers/streamer.py
@@ -6,20 +6,30 @@ from ..database.crud import VideoCrud
 from ..database.database import get_db
 from ..stream.stream import range_requests_response
 
+# meida path change to you local hard drive or media server
 media_path = os.getcwd()
-print(media_path)
 
+# Setup for fastapi router
 router = APIRouter(
     prefix="/stream/video",
     tags=["stream"],
     responses={404: {"description": "Not Found"}}
 )
 
+# Initalize the video crud object
 video_crud = VideoCrud()
 
 
 @router.get("/")
 async def video_stream(req: Request, video: str, db: Session = Depends(get_db)):
+    # req = getting the request header
+    # video = title of video
+    # db = database local session
+
+    # Checkin if the video path is in database
     db_video = video_crud.query_check(db, video, by_name=True)
+    # Create a complete path for the video
     path = os.path.join(media_path, db_video.path)
+    # Returning stream data in chunks as the video play
+    # with the request header information as the timestamp
     return range_requests_response(req, path, "video/mp4")
